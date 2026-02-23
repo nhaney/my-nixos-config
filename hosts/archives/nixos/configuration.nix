a95e2e2175dead889930ef309130dc42a19bd996
Nigel Haney <me@nigelhaney.com>
2026-02-22 21:12:51 -0800

    fix caddy hash.

--- hosts/archives/nixos/configuration.nix
+++ hosts/archives/nixos/configuration.nix
@@ -69,7 +69,7 @@
     enable = true;
     package = pkgs.caddy.withPlugins {
       plugins = [ "github.com/caddy-dns/cloudflare@v0.2.2" ];
-      hash = "sha256-GkmhBeHiuwdpRUDBPG9TRHqLvGnsxltPZMQ9CcRcdGA=";
+      hash = "sha256-dnhEjopeA0UiI+XVYHYpsjcEI6Y1Hacbi28hVKYQURg=";
     };
 
     virtualHosts = {
